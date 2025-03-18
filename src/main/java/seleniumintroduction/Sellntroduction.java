2e72e75cded8f293956c26315ba26955d298cf0e
madhu <madhuguntuboina1998@gmail.com>
2025-03-18 21:26:58 +0530

    First commit

---
+++ src/main/java/seleniumintroduction/Sellntroduction.java
@@ -0,0 +1,13 @@
+package seleniumintroduction;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sellntroduction {
+
+	public static void main(String[] args) {
+	 ChromeDriver driver= new ChromeDriver();
+	 
+
+	}
+
+}
