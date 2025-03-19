9136edd2cf9cbc84836cbd707db94425ae611e9f
madhu <madhuguntuboina1998@gmail.com>
2025-03-19 23:28:12 +0530

    commited by mb

--- src/main/java/seleniumintroduction/Sellntroduction.java
+++ src/main/java/seleniumintroduction/Sellntroduction.java
@@ -1,11 +1,13 @@
 package seleniumintroduction;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Sellntroduction {
 
 	public static void main(String[] args) {
-	 ChromeDriver driver= new ChromeDriver();
+	 WebDriver driver= (WebDriver) new ChromeDriver();
+	 driver.get("https://google.com");
 	 
 
 	}
