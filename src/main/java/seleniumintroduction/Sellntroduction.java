4bdd163af5b3ea24da228d5a7e88abed5c0971b3
madhu <madhuguntuboina1998@gmail.com>
2025-03-20 13:58:02 +0530

    changes

--- src/main/java/seleniumintroduction/Sellntroduction.java
+++ src/main/java/seleniumintroduction/Sellntroduction.java
@@ -1,11 +1,15 @@
 package seleniumintroduction;
-
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class Sellntroduction {
 
-	public static void main(String[] args) {
-	 ChromeDriver driver= new ChromeDriver();
+	public static <WebDriver> void main(String[] args) {
+		 WebDriver driver= (WebDriver) new ChromeDriver();
+		 driver.get("https://google.com");
+		 driver.getTitle();
+		 System.out.println("wfre");
+		 
+	 
 	 
 
 	}
